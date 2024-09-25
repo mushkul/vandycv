5a9aef8d48c58dae6e21120221b8bc182ee8ef6c
Luka <luka.mushkudiani@vanderbilt.edu>
2024-09-25 05:55:59 -0500

    Flask and Nextjs setup

---
+++ client/pages/index.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useState} from 'react'
+
+function index() {
+  const [message, setMessage] = useState("Loading");
+  const [people, setpeople] = useState([]);
+
+
+  useEffect(() =>{
+    fetch("http://localhost:8080/api/home/").then(
+      response => response.json()
+    ).then(
+      data => {
+        setMessage(data.message);
+        console.log(data.message);
+        setpeople(data.people);
+        console.log(data.people);
+      })
+    }, [])
+
+  return (
+    <div>
+      <div>{message}</div>
+      {
+        people.map((person, index) => (
+          <div key = {index}>
+            {person}
+          </div>
+        )
+        )
+      }
+
+    </div>
+  )
+}
+
+export default index;
