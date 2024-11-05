8d8b8e158191b4cf5109634cd3621dfe9e4dee36
Luka <luka.mushkudiani@vanderbilt.edu>
2024-11-04 22:57:08 -0600

    application name changed

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 from app import create_app
 
-app = create_app()
+application = create_app()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    application.run(debug=True, port=8080)
