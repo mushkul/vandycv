c16cc05cfec3f1765a33e455572551e802667f22
Luka <luka.mushkudiani@vanderbilt.edu>
2024-11-11 13:36:40 -0600

    app renamed

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 from app import create_app
 
-application = create_app()
+app = create_app()
 
 
 if __name__ == '__main__':
-    application.run(debug=True, port=8080)
+    app.run(debug=True, port=8080)
